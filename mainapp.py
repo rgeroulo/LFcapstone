516141002cb3bdf3c67fbe7618ec1e2cedf832e3
bdahdahee <39980621+bdahdahee@users.noreply.github.com>
2020-09-30 02:29:43 -0400

    Implemented a parser and displayed students in a scrollbar list

--- mainapp.py
+++ mainapp.py
@@ -2,41 +2,102 @@
 from tkinter import *
 from tkinter.filedialog import askopenfile
 from tkinter import messagebox
+import pandas as pd
+from IPython.display import display
+from csv import DictReader
+from LFparser import parser
+
+
+studentFileOpenCount = 0
 
 
 
-#setting up the GUI window and size
+#Setting up the GUI window and size of the initial window. The window can be dragged and altered to fit the desired size on the screen.
 root = Tk()
-root.geometry('500x350')
+root.title("The Learning Factory")
+root.geometry('850x700')
 
-#setting up the text on top of the window 
-mainTitle = Label(root, text= "Learning Factory Dashboard")
-subTitle = Label(root, text= "Please upload the proper CSV files to start")
+#################################################  MAIN FRAME  #################################################
+mainFrame = Frame(root)
+mainFrame.pack(side = TOP)
+mainFrameLabel = Label(mainFrame, text= "Learning Factory Dashboard", font = ("Courier", 35))
+mainFrameLabel.pack()
+subTitle = Label(mainFrame, text= "Please upload the proper CSV files to start", font = ("Courier", 20))
+subTitle.pack()
+
+
+#################################################  STUDENT FRAME  #################################################
+studentFrame = Frame(root)
+studentFrame.pack(side = LEFT, expand = TRUE, fill = BOTH)
+
+studentFrameLabel = Label(studentFrame, text = "Student Search", font = ("Courier", 20))
+studentFrameLabel.pack(side = TOP)
+
+studentSearchLabel = Label(studentFrame, text = "Student Name:", font = ("Courier", 10))
+studentSearchLabel.pack(side = TOP)
+
+studentSearch = Entry(studentFrame)
+studentSearch.pack(side = TOP)
+
+
+#################################################  PROJECT FRAME  #################################################
+projectFrame = Frame(root)
+projectFrame.pack(side = RIGHT, expand = TRUE, fill = BOTH)
+
+projectFrameLabel = Label(projectFrame, text = "Project Search", font = ("Courier", 20))
+projectFrameLabel.pack(side = TOP)
+
+projectSearchLabel = Label(projectFrame, text = "Project name:", font = ("Courier", 10))
+projectSearchLabel.pack(side = TOP)
+
+projectSearch = Entry(projectFrame)
+projectSearch.pack(side = TOP)
 
 
 
 #functions for the buttons on the home page
-
 #open_student_file and open_project_file allow the user to choose and open the desired CSV file. Upon upload the buttons for student list or project list will pop up
-def open_student_file(): 
-	global studentFile
-	studentFile = askopenfile(mode ='r+', filetypes =[('CSV Files', '*.csv')])
-	btn3.pack(side = TOP, pady = 10) 
+#def open_student_file(): 
+	#global studentFile
+	#studentFile = askopenfile(mode ='r+', filetypes =[('CSV Files', '*.csv')])
+	#btn3.pack(side = TOP, pady = 10) 
 
 
 def open_project_file(): 
 	global projectFile
 	projectFile = askopenfile(mode ='r+', filetypes =[('CSV Files', '*.csv')])
-	btn4.pack(side = TOP, pady = 10)
-	btn5.pack(side = TOP, pady = 10)
+	#btn4.pack(side = TOP, pady = 10)
+	#btn5.pack(side = TOP, pady = 10)
  
 #student and project list buttons allow the user to go to the list of students and projects in the future. For now it checks if the proper CSV file has been uploaded and shows content of CSV
 #file by setting offset at 0 and reading the file, if the file is missing an error will pop up
 def students_list():
+	global studentFile
+	global studentFileOpenCount
+	studentFile = askopenfile(mode ='r+', filetypes =[('CSV Files', '*.csv')])
+
 	if studentFile is not None:
+		#calls in parser to create a list of objects from CSV
+		studentlist = parser(studentFile)
+
+		#if a prevous CSV is open then the list will be deleted
+		if studentFileOpenCount != 0:
+			scrollbar.delete("1.0", tk.END)
+
+		#the scrollbar is implemented and filled with content 
+		scrollbar = Scrollbar(studentFrame)
+		scrollbar.pack(side = RIGHT, expand = TRUE, fill = BOTH)
+		lst = Listbox(studentFrame, yscrollcommand = scrollbar.set)
+		for obj in studentlist:
+			if obj.major != "Major":
+				lst.insert(END, obj.firstName + " " + obj.lastName)
+		lst.pack(side = LEFT, expand = TRUE, fill = BOTH)
+		scrollbar.config( command = lst.yview )
+		studentFileOpenCount += 1
 		studentFile.seek(0)
-		content = studentFile.read()
-		messagebox.showinfo("Student File", "Feature coming soon!\nFile contents:\n" + content)
+		#This display(content) is a pd function that displays the dataframe in the terminal.
+		# I need to inplement this so it shows in a message box.
+		#display(content)
 	else:
 		messagebox.showerror("Error", "No student CSV file detected")
 
@@ -44,26 +105,36 @@ def students_list():
 def project_list():
 	if projectFile is not None:
 		projectFile.seek(0)
-		content = projectFile.read()
-		messagebox.showinfo("Student File", "Feature coming soon!\nFile contents:\n" + content)
+		content = pd.read_csv(projectFile, delimiter=',')
+		#This display(content) is a pd function that displays the dataframe in the terminal.
+		# I need to inplement this so it shows in a message box.
+		display(content)
 	else:
 		messagebox.showerror("Error", "No project CSV file detected")
 
 
 
-#all the buttons that are on the homepage
-btn1 = Button(root, text ='Upload Student CSV', command = lambda:open_student_file())
-btn2 = Button(root, text ='Upload Project CSV', command = lambda:open_project_file()) 
-btn3 = Button(root, text ='Students List', command = students_list) 
-btn4 = Button(root, text ='Projects List', command = project_list)
-btn5 = Button(root, text ='Project Irregularity Test', command = project_list) 
+
+#All the buttons that are on the homepage in the format Button(root, text, command). Root is used to connect the button to the parents window. 
+# Text is used to display text on the button. Command is used to call a function when the button is clicked.
+btn1 = Button(mainFrame, text ='Upload Student CSV', command = students_list)
+btn2 = Button(mainFrame, text ='Upload Project CSV', command = lambda:open_project_file()) 
+#btn3 = Button(mainFrame, text ='List of Students', command = students_list) 
+btn4 = Button(mainFrame, text ='Project Irregularity Test', command = project_list)
+btn5 = Button(mainFrame, text ='Generate PDF', command = project_list) 
+
+btn1.pack(pady = 10)
+btn2.pack(pady = 10)
+#btn3.pack(pady = 10)
+btn4.pack(pady = 10)
+btn5.pack(pady = 10)
 
 
 #the initial packing of the homepage buttons 3-5 are packed into the homepage upon pressing of buttons 1-2
-mainTitle.pack(side = TOP, pady = 10)
-subTitle.pack(side = TOP, pady = 10)
-btn1.pack(side = TOP, pady = 10) 
-btn2.pack(side = TOP, pady = 10) 
+#mainTitle.pack(side = TOP, pady = 10)
+#subTitle.pack(side = TOP, pady = 10)
+#btn1.pack(side = TOP, pady = 10) 
+#btn2.pack(side = TOP, pady = 10) 
 
 
 #the main loop that keeps the app running 
---
+++ studentDataStructure.py
@@ -0,0 +1,19 @@
+#The student class contains all the attributes that a student would have.
+class Students:
+    def __init__(self, Major = '', ProjectID = '', TimeA = '',TimeB = '', TimeC = '', Comments = '', Student_NDA = '', Student_IP = '', campus_id = '', last_name = '', first_name = '', OnCampus = '',Var14 = ''):
+        self.major = Major
+        self.projectID = ProjectID
+        self.timeA = TimeA
+        self.timeB = TimeB
+        self.timeC = TimeC
+        self.comments = Comments
+        self.studentNDA = Student_NDA
+        self.studentIP = Student_IP
+        self.campusID = campus_id
+        self.lastName = last_name
+        self.firstName = first_name
+        self.onCampus = OnCampus
+        self.var14 = Var14
+
+
+
