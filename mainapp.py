109af211c026e1bdb8b1e4ecad3ce4a2ba0a14b4
bdahdahee <39980621+bdahdahee@users.noreply.github.com>
2020-12-15 22:17:07 -0500

    Added search bar to swap

--- mainapp.py
+++ mainapp.py
@@ -39,6 +39,23 @@ def student_search(event):
             continue
 
 
+def swap_search(event):
+    global student_filter
+    swap_filter = []
+    filter = event.widget.get().lower()
+    cursor = 0
+    #This line deletes the line in the csv that has the labels
+    stu_lst.delete(0, END)
+    #studentlst.insert(END, "First Name Last Name , Major , Project ID"))
+    for obj in studentlist[1:]:
+        if filter in obj.lastName.lower() or filter in obj.firstName.lower() or filter in obj.major.lower() or filter in obj.projectID.lower():
+            swap_filter.append((obj.firstName + ' ' + obj.lastName + ' ' + obj.major + ' ' + obj.projectID, cursor))
+            stu_lst.insert(END, obj.firstName + " " + obj.lastName + " , " + obj.major + " , " + obj.projectID)
+            cursor += 1
+        else:
+            cursor += 1
+            continue
+
 def project_search(event):
     # Enter a project's title, project ID, or comapny name in the entry and hit enter. This will filter out other projects' names
     # and only show relevant projects. These desired projects will be in the listbox. To go back to the full
@@ -108,7 +125,7 @@ filtered = len(studentSearch.get()) != 0
 
 #students_list will be used to display the students in a listbox in the bottom left corner
 #after the student CSV file is uploaded using the button in the middle of the GUI. This will
-#generate a search bar and a scroll bar as well.
+#generate a scroll bar as well.
 def students_list():
     global studentFile
     global studentFileOpenCount
@@ -127,7 +144,9 @@ def students_list():
 
         # the scrollbar is implemented and filled with content
         scrollbar = Scrollbar(studentFrame)
-        scrollbar.pack(side=RIGHT, expand=TRUE, fill = Y)
+        scrollbar.pack(side=RIGHT, fill = Y)
+        labeling = Label(studentFrame, text="Ordering: FirstName LastName , Major, Project ID")
+        labeling.pack(side=TOP)
         studentlst = Listbox(studentFrame, yscrollcommand=scrollbar.set)
         #studentlst.insert(END, "First Name Last Name , Major , Project ID")
         for obj in studentlist[1:]:
@@ -163,7 +182,9 @@ def project_list():
 
         # the scrollbar is implemented and filled with content
         scrollbar = Scrollbar(projectFrame)
-        scrollbar.pack(side=RIGHT, expand=TRUE, fill=Y)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        labeling = Label(projectFrame, text="Ordering: Project Title , Project ID, Company Name")
+        labeling.pack(side=TOP)
         projlst = Listbox(projectFrame, yscrollcommand=scrollbar.set)
         #The following line will add the titles for each column. If you ever change the
         #attributes being changed, make sure to change these titles as well.
@@ -185,10 +206,11 @@ def project_list():
 #just first name, just last name, major, 
 def student_select(event):
     global projid
+    global newWindow
 
     newWindow = Toplevel(root)
     newWindow.title("Student")
-    newWindow.geometry("800x400")
+    newWindow.geometry("400x400")
     Label(newWindow, text="Student window").pack()
     filtered = len(studentSearch.get()) != 0
     if filtered:
@@ -223,7 +245,7 @@ def project_select(event):
     # or move to a different project. Once this is completed, create a new CSV file and return to the user
     newWindow = Toplevel(root)
     newWindow.title("Project")
-    newWindow.geometry("400x600")
+    newWindow.geometry("400x550")
 
     projectPicked = projlst.curselection()
     projectPicked = projectPicked[0]
@@ -261,11 +283,14 @@ def project_select(event):
     courseName = Label(newWindow, text="Course Name: " + projectlist[projectPicked + 1].courseName)
     courseTime = Label(newWindow, text="Course Time: " + projectlist[projectPicked + 1].courseTime)
     physPrototype = Label(newWindow, text="Physical Prototype? " + projectlist[projectPicked + 1].physicalPrototype)
-    usedMajors = Label(newWindow, text="Majors Desired : " + str(needed))
+    desiredMajors = Label(newWindow, text="Majors Desired : " + str(needed))
 
-
+    '''
     btn1 = Button(newWindow, text='Swap teams with another student', command=swapStudents)
     btn2 = Button(newWindow, text='Move to a different team')
+    '''
+    #get the current majors that are needed
+
 
     name.pack(pady=10)
     company.pack(pady=10)
@@ -275,9 +300,11 @@ def project_select(event):
     courseName.pack(pady=10)
     courseTime.pack(pady=10)
     physPrototype.pack(pady=10)
-    usedMajors.pack(pady=10)
+    desiredMajors.pack(pady=10)
+    '''
     btn1.pack(pady=10)
     btn2.pack(pady=10)
+    '''
 
 
 def _delete_window():
@@ -296,8 +323,6 @@ def _destroy(event):
 
 
 def swap_select(event):
-    # Create a new window with the student attributes and 2 buttons to swap projects with another student
-    # or move to a different project.
     newWindow = Toplevel(root)
     newWindow.title("Student")
     newWindow.geometry("400x400")
@@ -347,9 +372,11 @@ def swap(swap_l):
     projid.config(text="Project ID : " + swap_id_two)
     studentlst.delete(0, END)
     for obj in studentlist[1:]:
-        studentlst.insert(END, obj.firstName + " " + obj.lastName + ", " + obj.major + ", " + obj.projectID)
+        studentlst.insert(END, obj.firstName + " " + obj.lastName + " , " + obj.major + " , " + obj.projectID)
     _delete_window()
-
+    newWindow.destroy()
+#This is the window that will open when you have selected a student and then have hit the "swap" 
+#button to choose another student to swap with.
 def swapStudents(swap_l):
     global _window_counter, studentFileOpenCount, stu_lst, pass_name, passed_index
     if _window_counter == 0:
@@ -362,11 +389,17 @@ def swapStudents(swap_l):
 
     student_change.protocol("WM_DELETE_WINDOW", _delete_window)
     student_change.bind("<Destroy>", _destroy)
-    student_change.title("Swapping Student")
+    student_change.title("Swapping Two Students")
     student_change.geometry("400x400")
 
+   
+    searchBar = Entry(student_change)
+    searchBar.bind('<Return>', swap_search)
+    searchBar.pack(side=TOP)
+
+
     scrollbar = Scrollbar(student_change)
-    scrollbar.pack(side=RIGHT, expand=TRUE, fill=BOTH)
+    scrollbar.pack(side=RIGHT, fill=Y)
     stu_lst = Listbox(student_change, yscrollcommand=scrollbar.set)
 
     pass_name = False
@@ -379,14 +412,14 @@ def swapStudents(swap_l):
             passed_index = index
             continue
         index += 1
-        stu_lst.insert(END, obj.firstName + " " + obj.lastName)
+        stu_lst.insert(END, obj.firstName + " " + obj.lastName + ", " + obj.major + ", " + obj.projectID)
     stu_lst.pack(side=LEFT, expand=TRUE, fill=BOTH)
     scrollbar.config(command=stu_lst.yview)
     studentFileOpenCount += 1
     stu_lst.bind('<Double-1>', swap_select)
 
-    swap_btn = Button(student_change, text='Swap with selected', command=lambda: swap(swap_l))
-    swap_btn.pack(pady=10)
+    confirmBtn = Button(student_change, text='Confirm Swap', command=lambda: swap(swap_l))
+    confirmBtn.pack(pady=10)
 
 
 # 1) Open another listbox with students to choose from and have the ability to double click and display that students
@@ -401,7 +434,7 @@ def move_select(event):
     # or move to a different project. Once this is completed, create a new CSV file and return to the user
     newWindow = Toplevel(root)
     newWindow.title("Project")
-    newWindow.geometry("800x800")
+    newWindow.geometry("400x400")
 
     projectPicked = proj_lst.curselection()[0]
 
@@ -413,6 +446,7 @@ def move_select(event):
     courseName = Label(newWindow, text="Course Name: " + projectlist[projectPicked + 1].courseName)
     courseTime = Label(newWindow, text="Course Time: " + projectlist[projectPicked + 1].courseTime)
     physPrototype = Label(newWindow, text="Physical Prototype? " + projectlist[projectPicked + 1].physicalPrototype)
+    '''
     bme = Label(newWindow, text="BME : " + projectlist[projectPicked + 1].bme)
     cmpen = Label(newWindow, text="CMPEN : " + projectlist[projectPicked + 1].cmpen)
     cmpsc = Label(newWindow, text="CMPSC : " + projectlist[projectPicked + 1].cmpsc)
@@ -424,6 +458,7 @@ def move_select(event):
     ie = Label(newWindow, text="IE : " + projectlist[projectPicked + 1].ie)
     matse = Label(newWindow, text="MATSE : " + projectlist[projectPicked + 1].matse)
     me = Label(newWindow, text="ME : " + projectlist[projectPicked + 1].me)
+    '''
 
     name.pack(pady=10)
     company.pack(pady=10)
@@ -433,6 +468,7 @@ def move_select(event):
     courseName.pack(pady=10)
     courseTime.pack(pady=10)
     physPrototype.pack(pady=10)
+    '''
     bme.pack(pady=10)
     cmpen.pack(pady=10)
     cmpsc.pack(pady=10)
@@ -444,6 +480,7 @@ def move_select(event):
     ie.pack(pady=10)
     matse.pack(pady=10)
     me.pack(pady=10)
+    '''
 
 
 def move(move_l):
@@ -457,8 +494,9 @@ def move(move_l):
     projid.config(text="Project ID : " + projectlist[projectPicked + 1].projectID)
     studentlst.delete(0, END)
     for obj in studentlist[1:]:
-        studentlst.insert(END, obj.firstName + " " + obj.lastName + ", " + obj.major + ", " + obj.projectID)
+        studentlst.insert(END, obj.firstName + " " + obj.lastName + " , " + obj.major + " , " + obj.projectID)
     _delete_window()
+
 
 def moveStudent(move_l):
     global _window_counter, projectFileOpenCount, proj_lst
@@ -476,7 +514,7 @@ def moveStudent(move_l):
     student_change.title("Moving Student")
 
     scrollbar = Scrollbar(student_change)
-    scrollbar.pack(side=RIGHT, expand=TRUE, fill=BOTH)
+    scrollbar.pack(side=RIGHT, fill=Y)
     proj_lst = Listbox(student_change, yscrollcommand=scrollbar.set)
 
     #Following line is the title for the listbox. If you change any attributes that are to be displayed.
@@ -490,7 +528,7 @@ def moveStudent(move_l):
     projectFileOpenCount += 1
     proj_lst.bind('<Double-1>', move_select)
 
-    move_btn = Button(student_change, text='move to selected', command=lambda: move(move_l))
+    move_btn = Button(student_change, text='Confirm Move', command=lambda: move(move_l))
     move_btn.pack(pady=10)
 
 #########################################################################################################
@@ -523,17 +561,28 @@ def team_irregularity():
         #open a new window for the irregularities
         newWindow2 = Toplevel(root)
         newWindow2.title("Irregularities")
-        newWindow2.geometry("800x400")
+        newWindow2.geometry("1200x600")
         #Split the new irregularities window into two frames, the left side will display the team
         #sizes and the right side will display teams that do not utilize all of the desired majors
         irrFrameLeft = Frame(newWindow2, width = 450)
         irrFrameLeft.pack(side=LEFT, expand=TRUE, fill=BOTH)
 
+        irrFrameMid = Frame(newWindow2, width = 450)
+        irrFrameMid.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+        irrFrameMid2 = Frame(newWindow2, width = 450)
+        irrFrameMid2.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
         irrFrameRight = Frame(newWindow2, width = 450)
         irrFrameRight.pack(side=RIGHT, expand=TRUE, fill=BOTH)
 
+        #Label for the left most frame which has the team sizes
+        sizeLabel = Label(irrFrameLeft, text= "TEAM SIZE")
+        sizeLabel.pack(pady=10)
+
         #This loop will iterate through the dictionary and display the counts for each
         #project ID and will calculate the min and max team size
+
         maxSize = 0
         minSize = 20
         for key in count:
@@ -583,17 +632,67 @@ def team_irregularity():
                     if obj.me != "0":
                         needed[projID].append("me")
         #Compare the "needed" and "majors" dictionaries to see if there are any disprecancies between
-        #what the project requires for majors and what majors the current students on the team have
-        difference = {}
+        #what the project requires for majors and what majors the current students on the team have.
+        #We will then display the majors that are needed but not on the team yet.
+        stillNeeded = {}
         for proj in needed:
             if proj in major.keys():
                 #Compare the majors for the project
-                difference[proj] = [item for item in needed[proj] if item not in major[proj]]
-            #The major needed was not found in the current list of students on the team so append
-            #This major to the difference list
-        for key, value in difference.items():
-            majorDifference = Label(irrFrameRight, text= "Majors not utilize for project ID " + key + " are " + str(value))
-            majorDifference.pack(pady=10)
+                stillNeeded[proj] = [item for item in needed[proj] if item not in major[proj]]
+                
+        #Check to see if there are nonrequired majors on the same project ID, if so, this can be useful
+        #for changing teams for you will know there are dispensable students on a team
+        extraNonNeeded = {}
+        for proj in major:
+            #If the projects majors are not in the needed majors dictionary, then add them to extraNonNeeded
+            extraNonNeeded[proj] = list(set(major[proj]) - set(needed[proj]))
+
+        #Check to see if there are multiple required majors on the same project ID, if so, this can be useful
+        #for changing teams for you will know there are dispensable students on a team
+        extraNeededMajors = {}
+        tmp = {}
+        for proj in needed:
+            total = {}
+            #Iterate through the needed majors for each project. Check to see how many occurances there
+            #are of the needed item in the current team. Add this number to total. This can help to show that there are
+            #multiple majors on a team that can be divided to other teams if needed.
+            for item in needed[proj]:
+                total[item] = major[proj].count(item)
+            #Add the counts of each major to the tmp dictioanry. tmp is a dictionary of dictionarys that holds the
+            #key = projectID value = a dictionary of majors to total total
+            tmp[proj] = total
+        
+        for proj, value in tmp.items():
+            extraNeededMajors[proj] = []
+            for key in value:
+                #If there are more than 1 required major for the class, display that info
+                if value[key] > 1:
+                    extraNeededMajors[proj].append(key)
+
+
+
+
+
+        stillNeededLabel = Label(irrFrameMid, text= "MAJORS STILL REQUIRED")
+        stillNeededLabel.pack(pady=10)
+
+        nonNeededExtraLabel = Label(irrFrameMid2, text = "EXTRA NON-REQUIRED MAJORS")
+        nonNeededExtraLabel.pack(pady=10)
+
+        neededExtraLabel = Label(irrFrameRight, text = "EXTRA REQUIRED MAJORS")
+        neededExtraLabel.pack(pady=10)
+        
+        for key, value in stillNeeded.items():
+            majorStillNeeded = Label(irrFrameMid, text= "Project ID " + key + " does not have a " + str(value))
+            majorStillNeeded.pack(pady=10)
+
+        for key, value in extraNonNeeded.items():
+            majorNonNeeded = Label(irrFrameMid2, text= "Project ID " + key + " does not need the " + str(value))
+            majorNonNeeded.pack(pady=10)
+
+        for key, value in extraNeededMajors.items():
+            majorExtra = Label(irrFrameRight, text= "Project ID " + key + " has extra required " + str(value))
+            majorExtra.pack(pady=10)
 
                 
     else:
@@ -615,15 +714,6 @@ def student_csv_output():
             student_writer.writerow([obj.major, obj.projectID, obj.timeA, obj.timeB, obj.timeC, obj.comments, obj.studentNDA, obj.studentIP, obj.campusID, obj.lastName, obj.firstName, obj.onCampus, obj.var14])
     else:
         messagebox.showerror("Error", "Please upload a student csv file first")
-def project_csv_output():
-    if projectFile is not None:
-        outputfile = fd.asksaveasfile(mode='w', defaultextension=".csv")
-        project_writer = csv.writer(outputfile, lineterminator = '\n')
-        project_writer.writerow(['ProjectID' , 'CompanyName' , 'ProjectTitle' , 'BME' , 'CMPEN' , 'CMPSC' , 'DS' , 'ED' , 'EE' , 'EGEE' , 'ESC' , 'IE' , 'MATSE' , 'ME' , 'Confidentiality' , 'IP' , 'CourseTime' , 'CourseName' , 'PhysicalPrototype'])
-        for obj in projectlist[1:]:
-            project_writer.writerow([obj.projectID , obj.companyName , obj.projectTitle  , obj.bme , obj.cmpen , obj.cmpsc , obj.ds , obj.ed , obj.ee , obj.egee , obj.esc , obj.ie , obj.matse , obj.me , obj.confidentiality , obj.ip , obj.courseTime , obj.courseName , obj.physicalPrototype])
-    else:
-        messagebox.showerror("Error", "Please upload a project csv file first")
 
 
 
@@ -634,13 +724,12 @@ btn1 = Button(mainFrame, text='Upload Student CSV', command=students_list)
 btn2 = Button(mainFrame, text='Upload Project CSV', command=project_list)
 btn4 = Button(mainFrame, text='Project Irregularity Test', command=team_irregularity)
 btn3 = Button(mainFrame, text='Export Student CSV', command=student_csv_output)
-btn5 = Button(mainFrame, text='Export Project CSV', command=project_csv_output)
 
 btn1.pack(pady=10)
 btn2.pack(pady=10)
 btn4.pack(pady=10)
 btn3.pack(pady=10)
-btn5.pack(pady=10)
+#btn5.pack(pady=10)
 
 # the main loop that keeps the app running
 mainloop()
